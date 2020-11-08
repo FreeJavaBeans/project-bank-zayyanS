9c13a454c033ab1b659fd1a287c205ccab9321a2
zayyanS <73193120+zayyanS@users.noreply.github.com>
2020-11-07 20:13:13 -0500

    Completed most of the user needs, just need to implement the test and logger

--- SavingsAccount.java
+++
@@ -1,5 +0,0 @@
-package bank.log;
-
-public class SavingsAccount {
-
-}
--- UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package bank.exceptions;
-
-public class UserNotFoundException extends Exception{
-	
-	public UserNotFoundException() {
-		super("User not found. Please try again or create an account.");
-	}
-
-}
