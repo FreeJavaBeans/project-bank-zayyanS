d7b78c7c66358be759b93b06e66d19d013a1c60f
zayyanS <73193120+zayyanS@users.noreply.github.com>
2020-11-10 11:02:25 -0500

    include logger

---
+++ projectbank/src/main/java/bank/logger/hub/ExampleLogger.java
@@ -0,0 +1,27 @@
+package bank.logger.hub;
+
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;  
+import org.apache.log4j.Logger;  
+import org.apache.log4j.PropertyConfigurator;
+
+public class ExampleLogger {
+
+		 private static final Logger logger = LogManager.getLogger(ExampleLogger.class);  
+	 public static void main(String[] args)throws IOException, SQLException {  
+	  // basic log4j configurator  
+	  PropertyConfigurator.configure("log4j.properties");  
+
+logger.debug("Sample debug message");
+      logger.info("Sample info message");
+      logger.error("Sample error message");
+      logger.fatal("Sample fatal message");
+      
+    
+	 }
+	 
+}
