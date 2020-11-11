6e395490c951b13bd154ec09ad1ee2e70bcb994d
zayyanS <73193120+zayyanS@users.noreply.github.com>
2020-11-10 22:32:56 -0500

    i need to remember to do git add . before commiting

---
+++ projectbank/src/main/java/bank/banker/hub/BankerStartUpPage.java
@@ -0,0 +1,58 @@
+package bank.banker.hub;
+
+import java.util.Scanner;
+
+import bank.employee.hub.EmployeeLogin;
+import bank.employee.hub.EmployeeRegistration;
+import bank.employee.hub.EmployeeStartupPage;
+import bank.exceptions.UserNotFoundException;
+import bank.user.hub.BankerRegistration;
+
+public class BankerStartUpPage {
+
+	private static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		try {
+			BankerStartUpPage esp = new BankerStartUpPage();
+		} catch (UserNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+public  BankerStartUpPage() throws UserNotFoundException {
+	super();
+	System.out.println("Hello. Welcome to the Starters Savings Savings Bank. We are a bank that caters to "
+			+ "young adults to teach them how \n to start saving their money in order to become a fiscally responsible "
+			+ "adult in the near future. Here at SSSB we \n cater to people between the ages of 10 and 19. We are the young adult safe banking way. "
+			+ "If you are between the \n years 16 and 19 on your signup date you will qualify for both a checking and savings account. At the point of \n signup each account will automatically have a gift of $5. Many of our customers sign up in store and can deposit \n more money right away."
+			+ " To continue and open an account with us, please press '1', if you would like to be directed \n to the login page please press '2'. If you would like to exit the application please press '3'. \n \n "
+			+ "1- Open a new account \n 2- Login \n 3- Exit");
+		int j = sc.nextInt();
+		switch(j) {
+		case 1:
+			BankerRegistration nbr= new BankerRegistration();
+			nbr.display();
+			
+			break;
+		case 2: 
+			BankerLogin ei = new BankerLogin();
+			ei.display();
+			
+			
+			break;
+		case 3: 
+			System.out.println("Application terminating. Goodbye.");
+		default:
+			break;
+		}
+
+	
+
+	
+
+	
+		
+	}}
+
+
